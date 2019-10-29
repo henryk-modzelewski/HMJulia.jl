c3537c78779adce3941b66b76c30379f40523ba3
Henryk Modzelewski <henryk_modzelewski@mac.com>
2019-10-29 14:31:11 -0700

    ...

--- src/HMJulia.jl
+++ src/HMJulia.jl
@@ -1,5 +1,15 @@
 module HMJulia
+    using IJulia
 
-greet() = print("Hello World!")
+    export mynotebook
+    notebooks = joinpath(ENV["HOME"],"Projects/Julia/Notebooks")
+    function mynotebook()
+        notebook(dir=joinpath(ENV["HOME"],notebooks))
+    end
+    function mynotebook(subdir::String)
+        newpath = joinpath(notebooks,subdir)
+        isdir(newpath) || mkdir(newpath)
+        notebook(dir=newpath)
+    end
 
 end # module
