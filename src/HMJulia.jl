809afd88fd02ae8d770749d16890558aa947c58c
Henryk Modzelewski <henryk_modzelewski@mac.com>
2020-04-06 13:03:50 -0700

    update_Julia

--- src/HMJulia.jl
+++ src/HMJulia.jl
@@ -1,20 +1,36 @@
 module HMJulia
 
+using Pkg
 using IJulia
 using InteractiveUtils
 
 ############################################################
 ## notebooks ###############################################
-export hm_notebook
-notebooks = joinpath(ENV["HOME"],"Projects/Julia/Notebooks")
-function hm_notebook()
-    notebook(dir=joinpath(ENV["HOME"],notebooks))
+export hm_update_Julia
+function hm_update_Julia()
+    try
+        Pkg.resolve()
+    catch
+        @warn "First resolve failed"
+    end
+    Pkg.update()
+    Pkg.resolve()
+    Pkg.gc()
 end
-function hm_notebook(subdir::String)
+
+############################################################
+## notebooks ###############################################
+export notebooks_hm
+notebooks_hm = joinpath(ENV["HOME"],"Projects/Julia/Notebooks")
+notebooks_HM() = notebook(dir=joinpath(notebooks))
+function notebooks_HM(subdir::String)
     newpath = joinpath(notebooks,subdir)
     isdir(newpath) || mkdir(newpath)
     notebook(dir=newpath)
 end
+export notebooks_softmeet
+notebooks_sm = joinpath(ENV["HOME"],"Projects/Julia/SLIM/Software-meeting/Julia-tutorials")
+notebooks_softmeet() = notebook(dir=notebooks_sm)
 
 ############################################################
 ## type tree ###############################################
